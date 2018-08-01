0a36e9a1de58e02a6808939043c19619f7dd1809
loskutyan <loskutov.alex@mail.ru>
2018-08-01 16:03:29 +0700

    models dao

--- dao/reactors.py
+++ dao/reactors.py
@@ -7,7 +7,7 @@ from model.reactor_schema import IsobutaneReactor, ReactorPlate
 class ReactorsDao:
     def __init__(self):
         path = os.path.abspath(os.path.dirname(__file__))
-        path = os.path.join(path, "../resources/reactors.json")
+        path = os.path.join(path, "../resources/dict/reactors.json")
         self._reactors_dict = {reactor_name: IsobutaneReactor([ReactorPlate(plate_name, sensors_config)
                                                                for plate_name, sensors_config in plates_config])
                                for reactor_name, plates_config in json.load(path).items()}
